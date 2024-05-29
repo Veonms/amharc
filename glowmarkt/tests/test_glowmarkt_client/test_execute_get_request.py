8c3ee59faffc457995493dd908d364a25c3476a8
Veonms <rwgalloway00@gmail.com>
2024-05-29 23:22:09 +0100

    Add tests for execute get request

---
+++ glowmarkt/tests/test_glowmarkt_client/test_execute_get_request.py
@@ -0,0 +1,120 @@
+import pytest
+import requests
+
+from glowmarkt.src.glowmarkt_client import GlowmarktClient
+
+
+class MockResponse:
+    def __init__(self, status_code: int, reason: str, token: str = None):
+        self.status_code = status_code
+        self.reason = reason
+        self.token = token
+
+    def json(self):
+        return {
+            "status_code": self.status_code,
+            "reason": self.reason,
+            "token": self.token,
+        }
+
+
+class MockSession:
+    def post(self, url: str, headers: dict, data: str) -> any:
+        pass
+
+    def get(self, url: str, headers: dict, params: dict) -> any:
+        pass
+
+
+def test_request_successful(monkeypatch: pytest.MonkeyPatch):
+    session = MockSession()
+
+    def mock_request(*args, **kwargs):
+        return MockResponse(status_code=200, reason="OK", token="test-token")
+
+    monkeypatch.setattr(
+        "test_execute_get_request.MockSession.get",
+        mock_request,
+    )
+
+    client = GlowmarktClient(
+        username="username",
+        password="password",
+        application_id="application_id",
+        session=session,
+    )
+
+    res = client._execute_get_request(url="", headers={}, params={})
+    assert res == {
+        "status_code": 200,
+        "reason": "OK",
+        "token": "test-token",
+    }
+
+
+def test_request_successful_params_none(monkeypatch: pytest.MonkeyPatch):
+    session = MockSession()
+
+    def mock_request(*args, **kwargs):
+        return MockResponse(status_code=200, reason="OK", token="test-token")
+
+    monkeypatch.setattr(
+        "test_execute_get_request.MockSession.get",
+        mock_request,
+    )
+
+    client = GlowmarktClient(
+        username="username",
+        password="password",
+        application_id="application_id",
+        session=session,
+    )
+
+    res = client._execute_get_request(url="", headers={})
+    assert res == {
+        "status_code": 200,
+        "reason": "OK",
+        "token": "test-token",
+    }
+
+
+def test_request_unsuccessful_non_200_code(monkeypatch: pytest.MonkeyPatch):
+    session = MockSession()
+
+    def mock_request(*args, **kwargs):
+        return MockResponse(status_code=404, reason="Not found", token="test-token")
+
+    monkeypatch.setattr(
+        "test_execute_get_request.MockSession.get",
+        mock_request,
+    )
+
+    client = GlowmarktClient(
+        username="username",
+        password="password",
+        application_id="application_id",
+        session=session,
+    )
+    with pytest.raises(requests.HTTPError):
+        client._execute_get_request(url="", headers={}, params={})
+
+
+def test_request_unsuccessful_non_200_code_params_none(monkeypatch: pytest.MonkeyPatch):
+    session = MockSession()
+
+    def mock_request(*args, **kwargs):
+        return MockResponse(status_code=404, reason="Not found", token="test-token")
+
+    monkeypatch.setattr(
+        "test_execute_get_request.MockSession.get",
+        mock_request,
+    )
+
+    client = GlowmarktClient(
+        username="username",
+        password="password",
+        application_id="application_id",
+        session=session,
+    )
+    with pytest.raises(requests.HTTPError):
+        client._execute_get_request(url="", headers={})
