811f9eff115450fb4e889e2a020efa5689265ef1
Veonms <rwgalloway00@gmail.com>
2024-05-30 19:26:28 +0100

    Add tests for retrieving readings.

---
+++ glowmarkt/tests/test_glowmarkt_client/test_retrieve_readings.py
@@ -0,0 +1,59 @@
+import pytest
+
+from glowmarkt.src.custom_exceptions.request_exceptions import NoReadingException
+from glowmarkt.src.data_model import Reading
+from glowmarkt.src.glowmarkt_client import GlowmarktClient
+from glowmarkt.tests.fixtures import MockSession
+
+
+def test_get_readings_successful(monkeypatch: pytest.MonkeyPatch):
+    session = MockSession()
+
+    def mock_request(*args, **kwargs):
+        return {"data": [["test-timestamp", 5.0]]}
+
+    monkeypatch.setattr(
+        "glowmarkt.src.glowmarkt_client.GlowmarktClient._execute_get_request",
+        mock_request,
+    )
+
+    client = GlowmarktClient(
+        username="username",
+        password="password",
+        application_id="application_id",
+        session=session,
+    )
+
+    client.token = "test-token"
+    client.veid = "test-veid"
+
+    assert client.retrieve_usage_readings(
+        resource_id="test-resource-id", from_date="test-date", to_date="test-date"
+    ) == [Reading(timestamp="test-timestamp", resourceId="test-resource-id", value=5.0)]
+
+
+def test_get_readings_unsuccessful_no_reading(monkeypatch: pytest.MonkeyPatch):
+    session = MockSession()
+
+    def mock_request(*args, **kwargs):
+        return {"no-data": []}
+
+    monkeypatch.setattr(
+        "glowmarkt.src.glowmarkt_client.GlowmarktClient._execute_get_request",
+        mock_request,
+    )
+
+    client = GlowmarktClient(
+        username="username",
+        password="password",
+        application_id="application_id",
+        session=session,
+    )
+
+    client.token = "test-token"
+    client.veid = "test-veid"
+
+    with pytest.raises(NoReadingException):
+        client.retrieve_usage_readings(
+            resource_id="test-resource-id", from_date="test-date", to_date="test-date"
+        )
