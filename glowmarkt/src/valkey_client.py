7534d1c3b25bac5dadac8ee4ebd404e791d16963
Veonms <rwgalloway00@gmail.com>
2024-06-04 19:03:45 +0100

    Removed unused code

--- glowmarkt/src/valkey_client.py
+++ glowmarkt/src/valkey_client.py
@@ -1,24 +1,37 @@
 import logging
-from typing import Optional
+from typing import Optional, Protocol
 
 import redis
 
 from glowmarkt.src.data_model import CachedCredentials
 
 
+class ValkeyPool(Protocol):
+    pass
+
+
+class ValkeyConnection(Protocol):
+    def set(self, name: str, value: str, ex: Optional[int] = None) -> None:
+        pass
+
+    def get(self, name: str) -> bytes:
+        pass
+
+    def close(self) -> None:
+        pass
+
+
 class ValkeyClient:
     def __init__(self, host: str, port: str, db: int = 0):
         self.pool: Optional[redis.ConnectionPool] = None
-        self.connection: Optional[redis.Redis] = None
+        self.connection: Optional[ValkeyConnection] = None
         self.host = host
         self.port = port
         self.db = db
 
     def create_connection(self):
-        self.pool = pool = redis.ConnectionPool(
-            host=self.host, port=self.port, db=self.db
-        )
-        self.connection = redis.Redis(connection_pool=pool)
+        self.pool = redis.ConnectionPool(host=self.host, port=self.port, db=self.db)
+        self.connection = redis.Redis(connection_pool=self.pool)
 
     def close_connection(self):
         if self.pool is not None:
@@ -38,8 +51,8 @@ class ValkeyClient:
 
     def get_credentials(self) -> Optional[CachedCredentials]:
         try:
-            token = self.connection.get("token")
-            veid = self.connection.get("veid")
+            token = self.connection.get(name="token")
+            veid = self.connection.get(name="veid")
         except Exception as err:
             logging.warning(f"Could not retrieve credentials: {err}")
             return None
@@ -57,7 +70,7 @@ class ValkeyClient:
 
     def get_delta(self, delta_key) -> Optional[str]:
         try:
-            delta = self.connection.get(delta_key)
+            delta = self.connection.get(name=delta_key)
         except Exception as err:
             logging.error(f"Could not retrieve the delta: {err}")
             return None
@@ -71,11 +84,3 @@ class ValkeyClient:
         except Exception as err:
             logging.error(f"Could not set the veid: {err}")
             raise err
-
-    def get_virtual_entity_id(self) -> Optional[str]:
-        try:
-            veid = self.connection.get("veid")
-        except Exception as err:
-            logging.error(f"Could not retrieve the veid: {err}")
-            return None
-        return veid
--- glowmarkt/tests/fixtures.py
+++ glowmarkt/tests/fixtures.py
@@ -1,6 +1,17 @@
+from typing import Optional
+
+
 class MockSession:
     def post(self, url: str, headers: dict, data: str) -> any:
         pass
 
     def get(self, url: str, headers: dict, params: dict) -> any:
+        pass
+
+
+class MockValkeyConnection:
+    def set(self, name: str, value: str, ex: Optional[int]):
+        pass
+
+    def get(self):
         pass
